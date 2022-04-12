ce6fc8ded9585746302e9226bc041bbda25fb419
unknown <gegitym@gmail.com>
2022-04-13 00:38:01 +0200

    piton

--- lesson 4/lesson 4.py
+++ lesson 4/lesson 4.py
@@ -1,1 +1,15 @@
-print("was wollen")
+name=str(input("Як вас звуть:"))
+surname=str(input("Яке у вас прізвище"))
+age=int(input("Скільки вам років:"))
+city=str(input("З якого ви міста\села:"))
+kraina=str(input("З якої ви країни:"))
+if(type(name==int)):
+    print("так не буває,ваше ім'я не ",name)
+elif(type(surname==int)):
+    print("так не буває, ваше прізвище не",surname)
+else:  
+    print("Ім'я: ",name)
+    print('Прізвище: ',surname)
+    print('Вік: ',age)
+    print('Місто\село:',city)
+    print('Країна: ',kraina)
